cc4bf922d64994d236c6ef211eacd3aaef64c646
yeropharaoh <yeroschutte@gmail.com>
2017-07-11 10:28:34 +0200

    resubmit

--- node/src/index.js
+++ node/src/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('public')); // Current directory is root
+app.use('/', express.static('../public')); // Current directory is root
 
 app.listen(3000, function(req,res){
 	console.log('Listening on port 3000');
